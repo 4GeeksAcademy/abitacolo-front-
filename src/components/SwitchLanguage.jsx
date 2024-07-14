a72ca59efd83877fee978b27292b1d1eb35afa9b
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-07-14 18:52:27 +0100

    optimizamos boton SwitchLanguage

--- src/components/SwitchLanguage.jsx
+++ src/components/SwitchLanguage.jsx
@@ -1,41 +1,32 @@
-import React, { useState } from "react"
-import { useTranslation } from "react-i18next"
-
-
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const SwitchLanguage = () => {
+  const { t, i18n } = useTranslation("global");
+  const [currentLanguage, setCurrentLanguage] = useState("es");
 
-    const [t, i18n] = useTranslation("global")
-    const [active , setIsActive] = useState(false)
+  const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setCurrentLanguage(lang);
+  };
 
-    const handleChangeLanguage = (lang) => {
-        i18n.changeLanguage(lang);
-        setIsActive(!active)
-
-       
-    }
-
-
-    return(
-        <div>
-            {active === true ? (
-            <>
-            <button className="font-bold" onClick={() => handleChangeLanguage("es")}>ESP</button>
-            <span>/</span>
-            <button onClick={() => handleChangeLanguage("en")}>ENG</button>
-            </> ):( 
-             <>   
-            <button  onClick={() => handleChangeLanguage("es")}>ESP</button>
-            <span>/</span>
-            <button className="font-bold" onClick={() => handleChangeLanguage("en")}>ENG</button>
-            </>
-            )}
-            
-        </div>
-    )
-
-}
-
-
+  return (
+    <div>
+      <button
+        className={currentLanguage === "es" ? "font-bold" : ""}
+        onClick={() => handleChangeLanguage("es")}
+      >
+        ESP
+      </button>
+      <span>/</span>
+      <button
+        className={currentLanguage === "en" ? "font-bold" : ""}
+        onClick={() => handleChangeLanguage("en")}
+      >
+        ENG
+      </button>
+    </div>
+  );
+};
 
 export default SwitchLanguage;
