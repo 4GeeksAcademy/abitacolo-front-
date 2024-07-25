389d5e351e7bb6cca2472ac659d44ae8dacbe01c
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-07-25 22:46:01 +0000

    ajuste padding

--- src/views/RegistroNuevoMueble.jsx
+++ src/views/RegistroNuevoMueble.jsx
@@ -218,7 +218,7 @@ function RegistroNuevoMueble() {
           type="number"
           name="precio_mes"
           id="floating_precio_mes"
-          className="block py-2.5  w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-abitacoloGreen -500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className="block py-2.5  w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-abitacoloGreen -500 focus:outline-none focus:ring-0 focus:border-abitacoloGreen -600 peer"
           placeholder=" "
           required
           value={formData.precio_mes}
