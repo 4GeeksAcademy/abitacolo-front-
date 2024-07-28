553b5c472fb7781e3ae903411d4e8ac56490b898
Guillermo C.G <guillermo.gil.cor@gmail.com>
2024-07-28 19:52:50 +0100

    user hardcode test uelz

--- src/context/flux.jsx
+++ src/context/flux.jsx
@@ -6,12 +6,14 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       user: {
-        email: "",
-        name: "",
-        password: "",
-        address: "",
-        nationality: "",
-        birth_date: "",
+        "address": null,
+    "birth_date": null,
+    "email": "qwe@qwe.com",
+    "favourites": [],
+    "id": 3,
+    "is_active": true,
+    "name": null,
+    "nationality": null
       },
       external_customer_id: "",
       carrito: [],
